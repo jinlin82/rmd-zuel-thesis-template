57dcd8031cf48deeb8ff2f01223db888a1d7fed1
Jin Lin <jinlin82@qq.com>
2020-07-27 08:28:30 +0800

    fix bookdown-pdf.bat

--- thesis/bookdown-pdf.bat
+++ thesis/bookdown-pdf.bat
@@ -8,7 +8,9 @@ for /f "delims=" %%a in ('dir *main.pdf /s /b *.sln') do set "name=%%~nxa"
 
 if exist .\_book\%name:~0,-4%.pdf (if exist .\_book\%name:~0,-4%.bak.pdf (del .\_book\%name:~0,-4%.bak.pdf)) 
 
-if exist .\_book\%name:~0,-4%.pdf (ren .\_book\%name:~0,-4%.pdf %name:~0,-4%.bak.pdf)
+REM 使用ren可以防止编译错误而打开以前生成的pdf，但是不会自动刷新，使用copy可以自动刷新，但是不能防止打开以前生成的pdf
+REM if exist .\_book\%name:~0,-4%.pdf (ren .\_book\%name:~0,-4%.pdf %name:~0,-4%.bak.pdf)
+if exist .\_book\%name:~0,-4%.pdf (copy .\_book\%name:~0,-4%.pdf .\_book\%name:~0,-4%.bak.pdf /y)
 
 Rscript %_SCRIPT_DRIVE%%_SCRIPT_PATH%bookdown-pdf.R %1
 
